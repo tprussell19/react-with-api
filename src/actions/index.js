92b2b70fc37b3014864a21c9015743124c386ad0
Thomas Russell <t.p.russell19@gmail.com>
2021-07-19 16:15:12 -0700

    BUGFIX: add actual error handling
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,39 +14,24 @@ export const getHeadlinesFailure = (error) => ({
   error
 });
 
-// write a function to handle the exception here, then call it in the .then block
-// const handle429 = (response) => {
-//   if (response.fault) return dispatch(getHeadlinesFailure(response.fault.faultstring))
-// }
-
 export const makeApiCall = () => {
   return dispatch => {
     dispatch(requestHeadlines);
     return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-      // if block
-      .then((response) => {
-        if (response.fault) {
-          return dispatch(getHeadlinesFailure(response.fault.faultstring))
-      } else {
+      .then(function(response) {
         return response.json()
-        // .then(handle429(response))
-        .then(
-          (jsonifiedResponse) => {
-            console.log(jsonifiedResponse)
-            dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
-          })
-        .catch((error) => {
-          dispatch(getHeadlinesFailure(error));
-        });
-      }})
+      })
+      .then(function(jsonifiedResponse) {
+        if (jsonifiedResponse.fault) {
+          console.log("Response.fault has evaluated to true");
+          throw Error(jsonifiedResponse.fault.faultstring);
+        } else {
+          console.log(jsonifiedResponse)
+          dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
+        }
+        })
+      .catch((error) => {
+        dispatch(getHeadlinesFailure(error));
+      });
   }
 }
-
-// {fault: {…}}
-// fault:
-//  detail:
-//    errorcode: "policies.ratelimit.QuotaViolation"
-// __proto__: Object
-//  faultstring: "Rate limit quota violation. Quota limit  exceeded. Identifier : 20bd1f0d-1c0f-495d-820c-28258ca8c175"
-// __proto__: Object
-// __proto__: Object
