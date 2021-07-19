3958744bcb61a926451e9c9721d9fd1819782770
Thomas Russell <t.p.russell19@gmail.com>
2021-07-19 15:46:14 -0700

    add error handling for bad requests
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,17 +14,39 @@ export const getHeadlinesFailure = (error) => ({
   error
 });
 
+// write a function to handle the exception here, then call it in the .then block
+// const handle429 = (response) => {
+//   if (response.fault) return dispatch(getHeadlinesFailure(response.fault.faultstring))
+// }
+
 export const makeApiCall = () => {
   return dispatch => {
     dispatch(requestHeadlines);
     return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-      .then(response => response.json())
-      .then(
-        (jsonifiedResponse) => {
-          dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
-        })
-      .catch((error) => {
-        dispatch(getHeadlinesFailure(error));
-      });
+      // if block
+      .then((response) => {
+        if (response.fault) {
+          return dispatch(getHeadlinesFailure(response.fault.faultstring))
+      } else {
+        return response.json()
+        // .then(handle429(response))
+        .then(
+          (jsonifiedResponse) => {
+            console.log(jsonifiedResponse)
+            dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
+          })
+        .catch((error) => {
+          dispatch(getHeadlinesFailure(error));
+        });
+      }})
   }
 }
+
+// {fault: {…}}
+// fault:
+//  detail:
+//    errorcode: "policies.ratelimit.QuotaViolation"
+// __proto__: Object
+//  faultstring: "Rate limit quota violation. Quota limit  exceeded. Identifier : 20bd1f0d-1c0f-495d-820c-28258ca8c175"
+// __proto__: Object
+// __proto__: Object
